e82dd2b50983fc815f4b0d0702b7cdec77176d16
leandrosax10 <leandrosantosti730@gmail.com>
2021-07-22 15:52:38 -0300

    api - Array e Listas - kotlin

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteintArray.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val values = IntArray( 5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+}
